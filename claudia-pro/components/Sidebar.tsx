72f2f5c6d01258f3f03fe0db33d9698543ee396a
xClaudiaArias <xClaudiaArias@gmail.com>
2025-04-03 11:21:46 -0400

    sidebar responsive and styled. needs more padding and margin next

--- claudia-pro/components/Sidebar.tsx
+++ claudia-pro/components/Sidebar.tsx
@@ -24,58 +24,58 @@ const Sidebar = () => {
     };
 
     return (
-        <div className="relative">
-            <div className="flex items-center justify-between pl-8 bg-white md:relative md:block">
-                <h2 className="text-lg font-bold py-1 px-2 bg-white md:relative md:block">
-                    Claudia Arias
+        <div className="relative border-solid border-2 border-red-700">
+            <div className="flex items-center justify-between pl-8 bg-white md:relative md:block border-solid border-2 border-orange-400">
+                <h2 className="lg:text-3xl uppercase font-bold py-4 px-2 bg-white md:relative md:block">
+                    Claudia 
+                    <br></br>
+                    Arias
                 </h2>
-                <div className="flex items-center justify-between p-2 bg-white md:hidden">
-                        <button
-                            title="menu"
-                            type="button"
-                            className="p-2 focus:outline-none"
-                            onClick={toggleSidebar}
-                        >
-                            <MenuIcon />
-                        </button>
+                <div className="flex items-center justify-between p-2 bg-white md:hidden border-solid border-2 border-lime-500">
+                    <button
+                        title="menu"
+                        type="button"
+                        className="p-2 focus:outline-none"
+                        onClick={toggleSidebar}
+                    >
+                        <MenuIcon />
+                    </button>
                 </div>
             </div>
 
 
             <aside className={`${
                     isOpen ? "block" : "hidden"
-                } md:block w-32 md:w-32 lg:w-64 h-screen bg-white pl-8 absolute md:relative top-0 right-0 z-50 md:h-auto`}
+                } md:block w-64 md:w-64 lg:w-64 h-80 lg:h-screen lg:bg-white pl-8 absolute md:relative right-0 z-50  md:pt-[200px] border-solid border-2 border-teal-500 lg:pt-0 md:bg-transparent`}
             >
 
-                <nav>
-                    <ul className="space-y-2">
+            <nav className="h-80 bg-white border-solid border-2 border-violet-400 rounded-lg shadow-md p-4 lg:rounded-none lg:shadow-none lg:p-0">
+                    <ul className="space-y-4">
                         <li>
-                            <Link href="/about" className="block p-2 hover:bg-rose-100 rounded">
+                            <Link href="/about" className="block p-2 hover:bg-rose-100 rounded text-gray-600">
                                 ABOUT
                             </Link>
                         </li>
                         <li>
-                            <Link href="/contact" className="block p-2 hover:bg-rose-100 rounded">
+                            <Link href="/contact" className="block p-2 hover:bg-rose-100 rounded text-gray-600">
                                 CONTACT
                             </Link>
                         </li>
                         <li>
-                            <Link href="/" className="block p-2 hover:bg-rose-100 rounded">
+                            <Link href="/" className="block p-2 hover:bg-rose-100 rounded text-gray-600">
                                 PROJECTS
                             </Link>
                         </li>
                         <li>
-                            <ul className="socials">
-                                <li><Link href="/"><WebAssetIcon></WebAssetIcon></Link></li>
-                                <li><Link href="/"><LinkedInIcon></LinkedInIcon></Link></li>
-                                <li><Link href="/"><GitHubIcon></GitHubIcon></Link></li>
-                                <li><Link href="/"><XIcon></XIcon></Link></li>
-                                <li><Link href="/"><InstagramIcon></InstagramIcon></Link></li>
+                            <ul className="socials flex space-x-4 p-2 gap-2 flex-wrap">
+                                <li><Link href="/"><WebAssetIcon className="text-gray-400"></WebAssetIcon></Link></li>
+                                <li><Link href="/"><LinkedInIcon className="text-gray-400"></LinkedInIcon></Link></li>
+                                <li><Link href="/"><GitHubIcon className="text-gray-400"></GitHubIcon></Link></li>
                             </ul>
                         </li>
-                        <li className="block p-2 hover:bg-rose-100 rounded" >
+                        <li className="block p-2 hover:bg-rose-100 rounded flex align-center" >
                             <button title="menu" type="button" className="md:hidden p-2" onClick={closeSidebar}>
-                                <CloseIcon/>
+                                <CloseIcon className="text-gray-300"/>
                             </button>
                         </li>
                     </ul>
